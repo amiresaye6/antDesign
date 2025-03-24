82d58ac2d9074505f27bdcdc74fefa536b7c6829
Amir Alsayed <amyralsyd367@gmail.com>
2025-03-24 02:12:26 +0200

    adding vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n/i18n.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
       <I18nextProvider i18n={i18n}>
         <BrowserRouter>
           <App />
+          <Analytics />
         </BrowserRouter>
       </I18nextProvider>
     </Provider>
